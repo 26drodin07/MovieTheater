ece2a19252c77f3245c93d3410992ad7366878a8
Дмитрий Родин <26drodin07@gmail.com>
2025-10-01 21:03:37 +0300

    feat(infrastructure): добавил dbcontext для postgres

---
+++ Infrastructure/AppDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Models;
+
+namespace Infrastructure
+{
+    public class AppDbContext : DbContext
+    {
+        DbSet<CinemaHall> CinemaHalls { get; set; }
+        DbSet<Movie> Movies { get; set; }
+        DbSet<MovieGenre> Genres { get; set; }
+        DbSet<MovieSession> Sessions { get; set; }
+        DbSet<PricePolicy> PricePolicies { get; set; }
+        DbSet<SalePolicy> SalePolicies { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+         : base(options)
+        {
+          
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+           
+        }
+    }
+}
