2e10a0122a3a8777ebc3b82873e03c8bae905dfe
Дмитрий Родин <26drodin07@gmail.com>
2025-10-03 10:35:36 +0300

    feat(application): методы расширения для получения цены сеанса

---
+++ Application/Extensions/MovieSessionExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Extensions
+{
+    public static class MovieSessionExtensions
+    {
+        public static decimal GetCurrentPrice(this MovieSession movieSession)
+        {
+            var pricePolicy = movieSession.GetActivePricePolicy();
+
+            return pricePolicy?.Price ?? movieSession.FallBackPrice;
+        }
+        public static PricePolicy? GetActivePricePolicy(this MovieSession movieSession)
+        {
+            return movieSession.PricePolices.SingleOrDefault(x =>
+            x.PolicyStart.ToDateTime(new()) > DateTime.UtcNow &&
+            (x.PolicyEnd == null || x.PolicyEnd?.ToDateTime(new()) < DateTime.UtcNow));
+        }
+    }
+}
