8e09a272f45926b5e015206dba54324ab895a0bf
Дмитрий Родин <26drodin07@gmail.com>
2025-10-03 11:59:47 +0300

    feat(core): dto для получения фильма

---
+++ Core/DTOs/MovieGetDTO.cs
@@ -0,0 +1,21 @@
+﻿using Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DTOs
+{
+    public record MovieGetDTO (  
+        int Id,
+        string Name,
+        int ReleaseDate,
+        bool IsInTheaters,
+        int PGRating,
+        TimeSpan Duration,
+        ICollection<GenreGetDTO> MovieGenres,
+        byte[]? Image,
+        MovieSessionGetDTO ClosestSession
+    );
+}
