b12bd88e360c63e438428e5263cbc90ef7c0515d
Дмитрий Родин <26drodin07@gmail.com>
2025-10-05 11:44:35 +0300

    feat(core): PatchDTOs

---
+++ Core/DTOs/Patch/MoviePatchDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DTOs.Patch
+{
+    public record MoviePatchDTO(
+    string? Name,
+    int? ReleaseDate,
+    bool? IsInTheaters,
+    int? PGRating,
+    TimeSpan? Duration,
+    ICollection<int>? MovieGenresIds);
+}
---
+++ Core/DTOs/Patch/SessionPatchDTO.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DTOs.Patch
+{
+    public record SessionPatchDTO(
+    int? MovieId,
+    int? CinemaHallId,
+    bool? IsCanceled,
+    DateTime? ActivationDate,
+    /// <summary>
+    /// Время начала (необходимо учитывать время работы кино, тех перерыв, длительность фильма)
+    /// </summary>
+    DateTime? StartTime,
+    /// <summary>
+    /// Сколько дней сеанс будет активен
+    /// </summary>
+    int? DurationInDays,
+    /// <summary>
+    /// Цена по умолчанию
+    /// </summary>
+    decimal? DefaultPrice);
+}
