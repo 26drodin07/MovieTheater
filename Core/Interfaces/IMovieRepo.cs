6a1c1ee4da9d6e590272b623082cdf68d8f59a07
Дмитрий Родин <26drodin07@gmail.com>
2025-10-05 11:45:58 +0300

    feat(core): Обновил интерфейсы

--- Core/Interfaces/IMovieRepo.cs
+++ Core/Interfaces/IMovieRepo.cs
@@ -1,4 +1,7 @@
 ﻿using Core.DTOs;
+using Core.DTOs.Get;
+using Core.DTOs.Patch;
+using Core.DTOs.Post;
 using Core.Models;
 using System;
 using System.Collections.Generic;
@@ -10,9 +13,34 @@ namespace Core.Interfaces
 {
     public interface IMovieRepo
     {
+        /// <summary>
+        /// Полный список фильмовя
+        /// </summary>
+        /// <returns></returns>
         Task<ICollection<MovieGetDTO>> GetMovies();
+        /// <summary>
+        /// Поиск с фильтрами
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="filters"></param>
+        /// <returns></returns>
         Task<ICollection<MovieGetDTO>> GetFiltered(string name, MovieFiltersDTO filters);
-       
+        /// <summary>
+        /// Снять с проката
+        /// </summary>
+        /// <param name="movieId"></param>
+        /// <returns></returns>
+        Task<MovieGetDTO> WithdrawFromDistibution(int movieId);
+
+        Task<MovieGetDTO> AddMovie(MoviePostDTO movie, byte[]? file);
+        Task<MovieGetDTO> UpdateMovie(MoviePatchDTO movie, byte[]? file, int id);
+        Task<int> RemoveMovie(int id);
+
+        
+        Task<ICollection<GenreGetDTO>> GetGenres();
+        Task<GenreGetDTO> AddGenre(GenrePostDTO genre);
+        Task<GenreGetDTO> UpdateGenre(GenrePostDTO genre, int id);
+
 
     }
 }
--- Core/Interfaces/IMovieSessionRepo.cs
+++ Core/Interfaces/IMovieSessionRepo.cs
@@ -1,4 +1,8 @@
-﻿using Core.Models;
+﻿using Core.DTOs.Get;
+using Core.DTOs.Patch;
+using Core.DTOs.Post;
+using Core.DTOs.Put;
+using Core.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,7 +13,20 @@ namespace Core.Interfaces
 {
     public interface IMovieSessionRepo
     {
-        Task<ICollection<MovieSession>> GetSessions();
-        Task<ICollection<MovieSession>> GetSessionsByMovie(int movieId);
+        Task<ICollection<MovieSessionGetDTO>> GetSessions();
+        Task<MovieSessionGetDTO> GetSessionById(int id);
+        Task<ICollection<MovieSessionGetDTO>> GetSessionsByMovie(int movieId);
+        Task<ICollection<SessionGroupedByFilmsDTO>> GetGroupedByFilm();
+        Task<MovieSessionGetDTO> AddSession(SessionPostDTO movieSession);
+        Task<MovieSessionGetDTO> UpdateSession(SessionPatchDTO movieSession);
+        Task<int> DeleteSession(int id);
+        //Кастомные цены
+        Task<MovieSessionGetDTO> AddCustomPrice(int sessionId, PricePolicyPostDTO priceDto);
+        Task<ICollection<PricePolicyGetDTO>> GetCustomPrices(int sessionId);
+        Task<PricePolicyGetDTO> UpdateCustomPrice(int id, int sessionId, PricePolicyPatchDTO priceDto);
+        Task<int> DeleteCustomPrice(int id);
+
+        Task<SalePolicyGetDTO> PutSale(SalePolicyPutDTO dto);
+        Task<ICollection<SalePolicyGetDTO>> GetSales(); 
     }
 }
