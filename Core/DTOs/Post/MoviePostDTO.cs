c4c059f528ec9f46f3217ab55dcbc72bd38d82c3
Дмитрий Родин <26drodin07@gmail.com>
2025-10-05 11:44:47 +0300

    feat(core): PostDTOs

---
+++ Core/DTOs/Post/MoviePostDTO.cs
@@ -0,0 +1,17 @@
+﻿using Core.DTOs.Get;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DTOs.Post
+{
+    public record MoviePostDTO(
+        string Name,
+        int ReleaseDate,
+        bool IsInTheaters,
+        int PGRating,
+        TimeSpan Duration,
+        ICollection<int> MovieGenresIds);
+}
---
+++ Core/DTOs/Post/SessionPostDTO.cs
@@ -0,0 +1,27 @@
+﻿using Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DTOs.Post
+{
+    public record SessionPostDTO(
+    int MovieId,
+    int CinemaHallId,
+    bool IsCanceled,
+    DateTime ActivationDate,
+    /// <summary>
+    /// Время начала (необходимо учитывать время работы кино, тех перерыв, длительность фильма)
+    /// </summary>
+    DateTime StartTime,
+    /// <summary>
+    /// Сколько дней сеанс будет активен
+    /// </summary>
+    int DurationInDays,
+    /// <summary>
+    /// Цена по умолчанию, которую выставляем при созддании сеанса
+    /// </summary>
+    decimal DefaultPrice);
+}
