1376aa4eab005a5d9c6a8530afcd29b6be51fd83
Дмитрий Родин <26drodin07@gmail.com>
2025-10-05 11:44:20 +0300

    feat(core): GetDTOs

---
+++ Core/DTOs/Get/MovieSessionGetDTO.cs
@@ -0,0 +1,22 @@
+﻿using Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DTOs.Get
+{
+    public record MovieSessionGetDTO
+    (
+        int Id,
+        int MovieId,
+        int CinemaHallId,
+        bool IsCanceled,
+        DateTime ActivationDate,
+        DateTime StartTime,
+        int DurationInDays,
+        decimal Price,
+        decimal DefaultPrice
+    );
+}
---
+++ Core/DTOs/Get/SalePolicyGetDTO.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DTOs.Get
+{
+    public record SalePolicyGetDTO (int Id, DateTime? startDate, decimal? saleCoof);
+
+}
